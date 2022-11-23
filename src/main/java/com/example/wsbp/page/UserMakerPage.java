2ff063d18ef7a2c348f5cb4c3aaa6670a346b3d3
lesson1 <b2201980@photon.chitose.ac.jp>
2022-11-24 00:42:31 +0900

    [2-1-2]フォームに入力したデータを送信できるようにした

--- src/main/java/com/example/wsbp/page/UserMakerPage.java
+++ src/main/java/com/example/wsbp/page/UserMakerPage.java
@@ -6,22 +6,39 @@ import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.Model;
 
 @MountPath("UserMaker")
 public class UserMakerPage extends WebPage {
 
     public UserMakerPage() {
+
+        var userNameModel = Model.of("");
+        var userPassModel = Model.of("");
+
         var toHomePageLink = new BookmarkablePageLink<>("toHome", HomePage.class);
         add(toHomePageLink);
 
-        var userInfoForm = new Form<>("userInfo");
+        var userInfoForm = new Form<>("userInfo"){
+            @Override
+            protected void onSubmit() {
+                var userName = userNameModel.getObject();
+                var userPass = userPassModel.getObject();
+                var msg = "送信データ："
+                        + userName
+                        + ","
+                        + userPass;
+                System.out.println(msg);
+            }
+        };
         add(userInfoForm);
 
-        var userNameField = new TextField<>("userName");
+        var userNameField = new TextField<>("userName", userNameModel);
         userInfoForm.add(userNameField);
 
-        var userPassField = new PasswordTextField("userPass");
+        var userPassField = new PasswordTextField("userPass", userPassModel);
         userInfoForm.add(userPassField);
+
     }
 
 }
