dd651f9f931ae87e9318b0e3339af533a777f4c6
lesson1 <b2201980@photon.chitose.ac.jp>
2022-11-23 13:05:07 +0900

    [3-課題]UserMakerPageからHomePageに戻るリンクを作成した

--- src/main/java/com/example/wsbp/page/UserMakerPage.java
+++ src/main/java/com/example/wsbp/page/UserMakerPage.java
@@ -2,11 +2,14 @@ package com.example.wsbp.page;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.wicketstuff.annotation.mount.MountPath;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 
 @MountPath("UserMaker")
 public class UserMakerPage extends WebPage {
 
     public UserMakerPage() {
+        var toHomePageLink = new BookmarkablePageLink<>("toHome", HomePage.class);
+        add(toHomePageLink);
 
     }
 
