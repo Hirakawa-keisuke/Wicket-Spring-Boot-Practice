245895ed375aa74043fe072e062b9ff9b0ee2397
lesson1 <b2201980@photon.chitose.ac.jp>
2022-11-24 00:37:58 +0900

    [2-1-1]ユーザ追加フォームを作成した

--- src/main/java/com/example/wsbp/page/UserMakerPage.java
+++ src/main/java/com/example/wsbp/page/UserMakerPage.java
@@ -3,6 +3,9 @@ package com.example.wsbp.page;
 import org.apache.wicket.markup.html.WebPage;
 import org.wicketstuff.annotation.mount.MountPath;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.TextField;
 
 @MountPath("UserMaker")
 public class UserMakerPage extends WebPage {
@@ -11,6 +14,14 @@ public class UserMakerPage extends WebPage {
         var toHomePageLink = new BookmarkablePageLink<>("toHome", HomePage.class);
         add(toHomePageLink);
 
+        var userInfoForm = new Form<>("userInfo");
+        add(userInfoForm);
+
+        var userNameField = new TextField<>("userName");
+        userInfoForm.add(userNameField);
+
+        var userPassField = new PasswordTextField("userPass");
+        userInfoForm.add(userPassField);
     }
 
 }
